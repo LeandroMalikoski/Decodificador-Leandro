24cb1bb46b60734f15c402104f882514f8cc2453
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2024-07-06 13:20:36 -0300

    1.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
 let codigoNormal = ['e', 'i', 'a', 'o', 'u'];
 let codigoCodificado = ['enter', 'imes', 'ai', 'ober', 'ufat'];
+let texto;
+
+document.getElementById('result').style.display = 'none';
+document.getElementById('button-copy').style.display = 'none';
+
+function result() {
+    let y = document.getElementById('fun-result');
+    y.setAttribute('class', 'aparecer');
+    document.getElementById('result').style.display = '';
+    document.getElementById('button-copy').style.display = '';
+    document.getElementById('result-img').style.display = 'none';
+    document.getElementById('result-text').style.display = 'none';
+}
 
 function getText() {
     return document.getElementById('getText').value;
@@ -10,11 +23,11 @@ function criptografar() {
     for (let i = 0; i < codigoNormal.length; i++) {
         texto = texto.replaceAll(codigoNormal[i], codigoCodificado[i]);
     }
-    if (texto.length === 0){
+    if (texto.length === 0) {
         alert('Insira um texto');
-    }
-    else {
-        alert(texto);
+    } else {
+        document.getElementById('result').innerHTML = texto;
+        result();
     }
     return texto;
 }
@@ -24,6 +37,12 @@ function descriptografar() {
     for (let i = 0; i < codigoNormal.length; i++) {
         texto = texto.replaceAll(codigoCodificado[i], codigoNormal[i]);
     }
-    alert(texto);
+    if (texto.length === 0) {
+        alert('Insira um texto');
+    } else {
+        document.getElementById('result').innerHTML = texto;
+        result();
+    }
     return texto;
 }
+
